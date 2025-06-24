d96fadb3f8a4c200fb4f9fc0ce348e119628ecc1
Geraldy Martin <geraldymp24@gmail.com>
2025-06-24 14:55:37 +0700

    add new data for area and booster

--- constants/boosters.tsx
+++ constants/boosters.tsx
@@ -2,21 +2,51 @@ import { IBooster } from '../types/IBooster';
 
 export const boosters: IBooster[] = [
   {
-    id: 'extra-hp',
-    name: 'Extra HP',
-    description: 'Increase your HP by 10',
-    apply: () => {} // logic handled later
+    id: 'restore-hp',
+    name: 'Restore HP',
+    description: 'Restore HP by 10',
+    apply: () => {}
   },
   {
     id: 'bonus-damage',
-    name: 'Bonus Damage',
-    description: 'Deal +2 extra damage each turn',
+    name: 'Generalist',
+    description: 'Increase total damage by 3',
+    apply: () => {}
+  },
+  {
+    id: 'max-reshuffle',
+    name: 'Everyday Shuffling',
+    description: 'Increase max Reshuffle by 1',
+    apply: () => {}
+  },
+  {
+    id: 'vowel-boost',
+    name: 'Vowel Up',
+    description: 'Every Vowel will deal +2 more damage',
+    apply: () => {}
+  },
+  {
+    id: 'abcde-boost',
+    name: 'Starter Kit',
+    description: 'Every ABCDE letter will deal +3 more damage',
+    apply: () => {}
+  },
+  {
+    id: 'vwxyz-boost',
+    name: 'Omega Bundle',
+    description: 'Every VWXYZ letter will deal +5 more damage',
+    apply: () => {}
+  },
+  {
+    id: 'ing-boost',
+    name: 'Working Class',
+    description: 'Word with ING will deal +8 more damage',
+    apply: () => {}
+  },
+  {
+    id: 'st-boost',
+    name: 'The Saint',
+    description: 'Every S and T letter will deal +4 more damage',
     apply: () => {}
   }
-  // {
-  //   id: 'gold-rush',
-  //   name: 'Gold Rush',
-  //   description: 'Gain +5 gold per battle win',
-  //   apply: () => {},
-  // },
 ];
