da3a3242fdd170b491dc00e4222b1315ab3e5d69
Geraldy Martin <geraldymp24@gmail.com>
2025-07-29 16:08:04 +0700

    rebalance health and damage of enemies

--- constants/enemies.tsx
+++ constants/enemies.tsx
@@ -14,25 +14,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Junior Mage',
         image: require('../assets/enemieses/forest/junior_mage.png'),
-        baseHp: 28,
-        minDmg: 7,
-        maxDmg: 10,
+        baseHp: 40,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       },
       {
         name: 'Living Tree',
         image: require('../assets/enemieses/forest/living_tree.png'),
-        baseHp: 24,
-        minDmg: 8,
-        maxDmg: 11,
+        baseHp: 42,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       },
       {
         name: 'Old Warlock',
         image: require('../assets/enemieses/forest/old_warlock.png'),
-        baseHp: 22,
-        minDmg: 9,
-        maxDmg: 12,
+        baseHp: 45,
+        minDmg: 5,
+        maxDmg: 7,
         goldReward: 10
       }
     ]
@@ -44,25 +44,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Big Ear Goblin',
         image: require('../assets/enemieses/goblin_camp/big_ear_goblin.png'),
-        baseHp: 44,
-        minDmg: 2,
-        maxDmg: 4,
+        baseHp: 55,
+        minDmg: 1,
+        maxDmg: 3,
         goldReward: 10
       },
       {
         name: 'Mad Goblin',
         image: require('../assets/enemieses/goblin_camp/mad_goblin.png'),
-        baseHp: 40,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 55,
+        minDmg: 2,
+        maxDmg: 3,
         goldReward: 10
       },
       {
         name: 'Scarred Goblin',
         image: require('../assets/enemieses/goblin_camp/scarred_goblin.png'),
-        baseHp: 36,
-        minDmg: 4,
-        maxDmg: 6,
+        baseHp: 60,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       }
     ]
@@ -74,25 +74,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Cracked Skull',
         image: require('../assets/enemieses/graveyard/cracked_skull.png'),
-        baseHp: 32,
-        minDmg: 10,
-        maxDmg: 13,
+        baseHp: 40,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       },
       {
         name: 'Dead Face',
         image: require('../assets/enemieses/graveyard/dead_face.png'),
-        baseHp: 28,
-        minDmg: 11,
-        maxDmg: 14,
+        baseHp: 42,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       },
       {
         name: 'The Frankenstein',
         image: require('../assets/enemieses/graveyard/the_frankenstein.png'),
-        baseHp: 26,
-        minDmg: 12,
-        maxDmg: 15,
+        baseHp: 45,
+        minDmg: 5,
+        maxDmg: 7,
         goldReward: 10
       }
     ]
@@ -104,25 +104,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Smilling Pumpkin',
         image: require('../assets/enemieses/witch_swamp/smilling_pumpkin.png'),
-        baseHp: 54,
-        minDmg: 4,
-        maxDmg: 7,
+        baseHp: 55,
+        minDmg: 1,
+        maxDmg: 3,
         goldReward: 10
       },
       {
         name: 'Bearded Centaur',
         image: require('../assets/enemieses/witch_swamp/bearded_centaur.png'),
-        baseHp: 50,
-        minDmg: 5,
-        maxDmg: 8,
+        baseHp: 55,
+        minDmg: 2,
+        maxDmg: 3,
         goldReward: 10
       },
       {
         name: 'The Medusa',
         image: require('../assets/enemieses/witch_swamp/the_medusa.png'),
-        baseHp: 46,
-        minDmg: 6,
-        maxDmg: 9,
+        baseHp: 60,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       }
     ]
@@ -134,25 +134,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Metal Knight',
         image: require('../assets/enemieses/ancient_kingdom/metal_knight.png'),
-        baseHp: 38,
-        minDmg: 14,
-        maxDmg: 18,
+        baseHp: 65,
+        minDmg: 2,
+        maxDmg: 3,
         goldReward: 10
       },
       {
         name: 'Old Viking',
         image: require('../assets/enemieses/ancient_kingdom/old_viking.png'),
-        baseHp: 34,
-        minDmg: 15,
-        maxDmg: 19,
+        baseHp: 60,
+        minDmg: 2,
+        maxDmg: 4,
         goldReward: 10
       },
       {
         name: 'The Executioner',
         image: require('../assets/enemieses/ancient_kingdom/the_executioner.png'),
-        baseHp: 30,
-        minDmg: 16,
-        maxDmg: 20,
+        baseHp: 65,
+        minDmg: 3,
+        maxDmg: 5,
         goldReward: 10
       }
     ]
@@ -164,25 +164,25 @@ export const enemies: IEnemies[] = [
       {
         name: 'Revamped Android',
         image: require('../assets/enemieses/futuristic_city/revamped_android.png'),
-        baseHp: 70,
-        minDmg: 7,
-        maxDmg: 11,
+        baseHp: 46,
+        minDmg: 5,
+        maxDmg: 7,
         goldReward: 10
       },
       {
         name: 'Half Cyborg',
         image: require('../assets/enemieses/futuristic_city/half_cyborg.png'),
-        baseHp: 64,
-        minDmg: 8,
-        maxDmg: 12,
+        baseHp: 48,
+        minDmg: 5,
+        maxDmg: 7,
         goldReward: 10
       },
       {
         name: 'Ancient Machine',
         image: require('../assets/enemieses/futuristic_city/ancient_machine.png'),
-        baseHp: 58,
+        baseHp: 50,
         minDmg: 9,
-        maxDmg: 13,
+        maxDmg: 9,
         goldReward: 10
       }
     ]
@@ -194,9 +194,9 @@ export const enemies: IEnemies[] = [
       {
         name: 'The Death',
         image: require('../assets/enemieses/the_death.png'),
-        baseHp: 120,
-        minDmg: 18,
-        maxDmg: 25,
+        baseHp: 70,
+        minDmg: 10,
+        maxDmg: 15,
         goldReward: 10
       }
     ]
