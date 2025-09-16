94f685dae0ab4929f5505765b2e96e7af41fbc19
Geraldy Martin <geraldymp24@gmail.com>
2025-09-16 10:01:58 +0700

    update ad unit to proper one

--- lib/ads/config.tsx
+++ lib/ads/config.tsx
@@ -3,8 +3,8 @@ import { Platform } from 'react-native';
 import { TestIds } from 'react-native-google-mobile-ads';
 
 // Replace with your real Rewarded Ad Unit IDs when ready
-const REAL_ANDROID = process.env.ADMOB_APPID;
-const REAL_IOS = process.env.ADMOB_APPID;
+const REAL_ANDROID = 'ca-app-pub-2952486024858170/7370634530';
+const REAL_IOS = 'ca-app-pub-2952486024858170/7370634530';
 
 export const REWARDED_UNIT_ID = __DEV__
   ? TestIds.REWARDED
