b7ecb726f843ee61619573604d9f1926c87c928f
Geraldy Martin <geraldymp24@gmail.com>
2025-09-23 20:00:31 +0700

    add new contents for tutorial modal

---
+++ constants/tutorial_contents.tsx
@@ -0,0 +1,38 @@
+export const TutorialContents = [
+  {
+    id: 'how',
+    title: 'How to play',
+    description: `Select the letters to create a word and click bottom right button to damage the enemy.`,
+    image: require('@assets/tutorial/tutorial_1.png')
+  },
+  {
+    id: 'enemy_attack',
+    title: 'Enemy will attack back',
+    description: `The enemy will hit you back after you damage them. The damage varies on the enemy status`,
+    image: require('@assets/tutorial/tutorial_2.png')
+  },
+  {
+    id: 'enemy_beaten',
+    title: 'Beat the enemy quickly',
+    description: `Beat enemy to gain mana and progress to next battle`,
+    image: require('@assets/tutorial/tutorial_3.png')
+  },
+  {
+    id: 'careful',
+    title: 'Care for yourself',
+    description: `Watch your HP. Game will be over if it drop to Zero`,
+    image: require('@assets/tutorial/tutorial_4.png')
+  },
+  {
+    id: 'reshuffle',
+    title: 'Reshuffle and Rearrange',
+    description: `You have ability to get new sets of letter using Reshuffle. And you can change position of current letters with Rearrange, to give you new perspective`,
+    image: require('@assets/tutorial/tutorial_5.png')
+  },
+  {
+    id: 'good_luck',
+    title: 'Good luck and have a great journey!',
+    description: `Now move forward and fight the enemy! The world needs you!`,
+    image: require('@assets/tutorial/tutorial_6.png')
+  }
+];
