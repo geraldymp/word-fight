13d5f1bbb0d47aea9d50a4e3c1d5403d4914ae19
Geraldy Martin <geraldymp24@gmail.com>
2025-09-23 12:41:08 +0700

    remove unused files

--- utils/calculateDamage.tsx
+++
@@ -1,9 +0,0 @@
-// TODO: Remove this after nothing use this
-import { letterDamageMap } from '@constants/letterDamage';
-
-export const calculateBaseLetterDamage = (word: string): number => {
-  return word
-    .toUpperCase()
-    .split('')
-    .reduce((total, letter) => total + (letterDamageMap[letter] || 0), 0);
-};
