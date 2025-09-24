0cf40f7e4d6676988223bfe889e97e5b214208fc
Geraldy Martin <geraldymp24@gmail.com>
2025-09-24 20:58:13 +0700

    add function to subscribe via revenue cat

---
+++ store/useSubscriptionStore.tsx
@@ -0,0 +1,29 @@
+// store/useSubscriptionStore.ts
+import Purchases, { CustomerInfo } from 'react-native-purchases';
+import { create } from 'zustand';
+
+type SubscriptionState = {
+  isPremium: boolean;
+  setFromCustomerInfo: (info: CustomerInfo) => void;
+  refresh: () => Promise<void>;
+};
+
+export const useSubscriptionStore = create<SubscriptionState>(set => ({
+  isPremium: false,
+
+  setFromCustomerInfo: info => {
+    const premiumActive = info.entitlements.active['monthly_pro'] !== undefined;
+    set({ isPremium: premiumActive });
+  },
+
+  refresh: async () => {
+    try {
+      const info = await Purchases.getCustomerInfo();
+      const premiumActive =
+        info.entitlements.active['monthly_pro'] !== undefined;
+      set({ isPremium: premiumActive });
+    } catch (e) {
+      console.warn('Failed to refresh subscription', e);
+    }
+  }
+}));
