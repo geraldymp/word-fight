a8520a65d7e3068746239bc97230df6fdf645390
Geraldy Martin <geraldymp24@gmail.com>
2025-09-24 21:43:06 +0700

    update versionCode to 11

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: 'Word Fight',
     slug: 'word-fight',
-    version: '1.0.0',
+    version: '1.0.11',
     orientation: 'portrait',
     icon: './assets/word_fight_logo.png',
     scheme: 'wordfight',
@@ -16,7 +16,7 @@ export default {
       bundleIdentifier: 'com.germp24.wordfight'
     },
     android: {
-      versionCode: 10,
+      versionCode: 11,
       package: "com.germp24.wordfight",
       adaptiveIcon: {
         foregroundImage: './assets/word_fight_logo.png',
