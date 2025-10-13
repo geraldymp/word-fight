ff84e575dc663c5b88c44c7d0912ea45d739fbcb
Geraldy Martin <geraldymp24@gmail.com>
2025-10-13 09:46:21 +0700

    update version to 17

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default {
   expo: {
     name: 'Word Fight',
     slug: 'word-fight',
-    version: '1.0.16',
+    version: '1.0.17',
     orientation: 'portrait',
     icon: './assets/word_fight_logo.png',
     scheme: 'wordfight',
@@ -16,7 +16,7 @@ export default {
       bundleIdentifier: 'com.germp24.wordfight'
     },
     android: {
-      versionCode: 16,
+      versionCode: 17,
       package: "com.germp24.wordfight",
       adaptiveIcon: {
         foregroundImage: './assets/word_fight_logo.png',
