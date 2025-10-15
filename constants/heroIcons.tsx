c1df232e6aab0b22f53ef3be102b4df8840f6b39
Geraldy Martin <geraldymp24@gmail.com>
2025-10-15 17:05:44 +0700

    add constants for hero icons

---
+++ constants/heroIcons.tsx
@@ -0,0 +1,32 @@
+export const HeroIcons = [
+  {
+    id: 'bald_warrior',
+    name: 'Rodrygo',
+    icon: require('@assets/hero_images/bald_warrior.png')
+  },
+  {
+    id: 'bearded_warrior',
+    name: 'Shamrock',
+    icon: require('@assets/hero_images/bearded_warrior.png')
+  },
+  {
+    id: 'female_mage',
+    name: 'Sarah',
+    icon: require('@assets/hero_images/female_mage.png')
+  },
+  {
+    id: 'female_pirate',
+    name: 'Helena',
+    icon: require('@assets/hero_images/female_pirate.png')
+  },
+  {
+    id: 'female_thief',
+    name: 'Assurah',
+    icon: require('@assets/hero_images/female_thief.png')
+  },
+  {
+    id: 'male_archer',
+    name: 'Hylos',
+    icon: require('@assets/hero_images/male_archer.png')
+  }
+];
