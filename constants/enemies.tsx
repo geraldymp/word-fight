89cc2f24d75beebb59a4cad20eacb775e293e8db
Geraldy Martin <geraldymp24@gmail.com>
2025-11-06 14:19:07 +0700

    rebalance enemies damage and hp

--- constants/enemies.tsx
+++ constants/enemies.tsx
@@ -14,27 +14,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'juniorMage',
         name: 'Junior Mage',
-        baseHp: 36,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 280,
+        minDmg: 20,
+        maxDmg: 30,
         minManaBounty: 5,
         maxManaBounty: 8
       },
       {
         id: 'livingTree',
         name: 'Living Tree',
-        baseHp: 38,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 300,
+        minDmg: 20,
+        maxDmg: 40,
         minManaBounty: 6,
         maxManaBounty: 9
       },
       {
         id: 'oldWarlock',
         name: 'Old Warlock',
-        baseHp: 40,
-        minDmg: 5,
-        maxDmg: 7,
+        baseHp: 320,
+        minDmg: 20,
+        maxDmg: 50,
         minManaBounty: 7,
         maxManaBounty: 10
       }
@@ -47,27 +47,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'scarredGoblin',
         name: 'Scarred Goblin',
-        baseHp: 45,
-        minDmg: 1,
-        maxDmg: 3,
+        baseHp: 380,
+        minDmg: 10,
+        maxDmg: 20,
         minManaBounty: 4,
         maxManaBounty: 9
       },
       {
         id: 'oneEyedGoblin',
         name: 'One-eyed Goblin',
-        baseHp: 48,
-        minDmg: 2,
-        maxDmg: 3,
+        baseHp: 400,
+        minDmg: 10,
+        maxDmg: 30,
         minManaBounty: 5,
         maxManaBounty: 10
       },
       {
         id: 'madGoblin',
         name: 'Mad Goblin',
-        baseHp: 50,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 420,
+        minDmg: 10,
+        maxDmg: 40,
         minManaBounty: 6,
         maxManaBounty: 11
       }
@@ -80,27 +80,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'crackedSkull',
         name: 'Cracked Skull',
-        baseHp: 41,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 290,
+        minDmg: 25,
+        maxDmg: 35,
         minManaBounty: 5,
         maxManaBounty: 8
       },
       {
         id: 'deadFace',
         name: 'Dead Face',
-        baseHp: 42,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 310,
+        minDmg: 25,
+        maxDmg: 45,
         minManaBounty: 6,
         maxManaBounty: 9
       },
       {
         id: 'theFrankenstein',
         name: 'The Frankenstein',
-        baseHp: 45,
-        minDmg: 5,
-        maxDmg: 7,
+        baseHp: 330,
+        minDmg: 25,
+        maxDmg: 55,
         minManaBounty: 7,
         maxManaBounty: 10
       }
@@ -113,27 +113,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'greenMoss',
         name: 'Green Moss',
-        baseHp: 55,
-        minDmg: 1,
-        maxDmg: 3,
+        baseHp: 390,
+        minDmg: 15,
+        maxDmg: 25,
         minManaBounty: 4,
         maxManaBounty: 9
       },
       {
         id: 'beardedCentaur',
         name: 'Bearded Centaur',
-        baseHp: 55,
-        minDmg: 2,
-        maxDmg: 3,
+        baseHp: 410,
+        minDmg: 15,
+        maxDmg: 35,
         minManaBounty: 5,
         maxManaBounty: 10
       },
       {
         id: 'theMedusa',
         name: 'The Medusa',
-        baseHp: 60,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 430,
+        minDmg: 15,
+        maxDmg: 45,
         minManaBounty: 6,
         maxManaBounty: 11
       }
@@ -146,27 +146,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'metalKnight',
         name: 'Metal Knight',
-        baseHp: 60,
-        minDmg: 2,
-        maxDmg: 3,
+        baseHp: 320,
+        minDmg: 30,
+        maxDmg: 40,
         minManaBounty: 8,
         maxManaBounty: 12
       },
       {
         id: 'oldViking',
         name: 'Old Viking',
-        baseHp: 60,
-        minDmg: 2,
-        maxDmg: 4,
+        baseHp: 340,
+        minDmg: 30,
+        maxDmg: 50,
         minManaBounty: 9,
         maxManaBounty: 13
       },
       {
         id: 'theExecutioner',
         name: 'The Executioner',
-        baseHp: 65,
-        minDmg: 3,
-        maxDmg: 5,
+        baseHp: 360,
+        minDmg: 30,
+        maxDmg: 60,
         minManaBounty: 10,
         maxManaBounty: 15
       }
@@ -179,27 +179,27 @@ export const enemies: IEnemies[] = [
       {
         id: 'halfCyborg',
         name: 'Half Cyborg',
-        baseHp: 46,
-        minDmg: 5,
-        maxDmg: 7,
+        baseHp: 420,
+        minDmg: 20,
+        maxDmg: 30,
         minManaBounty: 10,
         maxManaBounty: 11
       },
       {
         id: 'gunnerMachine',
         name: 'Gunner Machine',
-        baseHp: 48,
-        minDmg: 5,
-        maxDmg: 7,
+        baseHp: 440,
+        minDmg: 20,
+        maxDmg: 40,
         minManaBounty: 11,
         maxManaBounty: 12
       },
       {
         id: 'flyingAndroid',
         name: 'Flying Android',
-        baseHp: 50,
-        minDmg: 9,
-        maxDmg: 9,
+        baseHp: 460,
+        minDmg: 20,
+        maxDmg: 50,
         minManaBounty: 13,
         maxManaBounty: 14
       }
@@ -212,9 +212,9 @@ export const enemies: IEnemies[] = [
       {
         id: 'boss1',
         name: 'Azazel the Archmage',
-        baseHp: 70,
-        minDmg: 10,
-        maxDmg: 15,
+        baseHp: 500,
+        minDmg: 35,
+        maxDmg: 65,
         minManaBounty: 50,
         maxManaBounty: 50
       }
