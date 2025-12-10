d2665d9b0f8aeaeb22108955b58e5b519ec91b1a
Geraldy Martin <geraldymp24@gmail.com>
2025-12-10 13:58:00 +0700

    change font to const for reusability

---
+++ foundation/fonts.tsx
@@ -0,0 +1,12 @@
+export const Fonts = {
+  ArchitectsDaughter400: 'ArchitectsDaughter_400Regular',
+  Candal400: 'Candal_400Regular',
+  Chilanka400: 'Chilanka_400Regular',
+  Cinzel400: 'Cinzel_400Regular',
+  DaysOne400: 'DaysOne_400Regular',
+  GoblinOne400: 'GoblinOne_400Regular',
+  SourGummy400: 'SourGummy_400Regular',
+  SourGummy800: 'SourGummy_800ExtraBold'
+} as const;
+
+export type FontKeys = keyof typeof Fonts;
